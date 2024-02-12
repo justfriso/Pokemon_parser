845bfd97583aa58ca21ab27f426be95883eb117e
justfriso <133640650+justfriso@users.noreply.github.com>
2024-02-12 14:47:20 +0100

    scraper now writes to txt file
    added feature that writes the text into a txt file and a feature that reads the txt file to confirm it actually exists and to see how it is written in the file.

--- scraper_pokemon.py
+++ scraper_pokemon.py
@@ -1,18 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.serebii.net/pokemon/nationalpokedex.shtml'
+
 """
 writing a program to determine what types are most common, what type combos
 do not yet exist and what types are most common in which region using the
 current(februari 2024) pokedex from "https://www.serebii.net/pokemon/nationalpokedex.shtml"
 """
 
-from bs4 import BeautifulSoup
-import requests
-
-url = 'https://www.serebii.net/pokemon/nationalpokedex.shtml'
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-article_titles = soup.find_all('td', class_="fooinfo")
+pokemon_attributes = soup.find_all('td', class_="fooinfo")
 
-for title in article_titles:
-    print(title.text)
+with open('pokemon_attributes.txt', 'w', encoding='utf-8') as file:
+    print("Pokemon Attributes:", file=file)
+    for attribute in pokemon_attributes:
+        print(attribute.text, file=file)
+
+
+with open('pokemon_attributes.txt', 'r') as file:
+    lines = file.readlines()
+
+
+for line in lines:
+    print(line.strip()) 
